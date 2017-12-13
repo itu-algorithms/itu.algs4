08aa515bd69945886bbc87e6e911a22fc7f0722b
Otto <otcl@itu.dk>
2017-12-13 22:56:58 +0100

    Changed from static methods to functions

--- selection.py
+++ selection.py
@@ -1,24 +1,22 @@
-class Selection:
-    @staticmethod
-    def sort(a):
-        _n = len(a)
-        for i in range(_n):
-            _min = i
-            for j in range(i+1, _n):
-                if a[j] < a[_min]:
-                    _min = j
-            a[i], a[_min] = a[_min], a[i]
+def sort(a):
+    _n = len(a)
+    for i in range(_n):
+        _min = i
+        for j in range(i+1, _n):
+            if a[j] < a[_min]:
+                _min = j
+        a[i], a[_min] = a[_min], a[i]
 
-    @staticmethod
-    def sort_with_comparator(a, cmp):
-        _n = len(a)
-        for i in range(_n):
-            _min = i
-            for j in range(i + 1, _n):
-                if Selection._less(a[j], a[_min], cmp):
-                    _min = j
-            a[i], a[_min] = a[_min], a[i]
 
-    @staticmethod
-    def _less(v, w, cmp):
-        return cmp(v, w) < 0
+def sort_cmp(a, cmp):
+    _n = len(a)
+    for i in range(_n):
+        _min = i
+        for j in range(i + 1, _n):
+            if _less(a[j], a[_min], cmp):
+                _min = j
+        a[i], a[_min] = a[_min], a[i]
+
+
+def _less(v, w, cmp):
+    return cmp(v, w) < 0
