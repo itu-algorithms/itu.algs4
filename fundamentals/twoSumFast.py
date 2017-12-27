8e1280a45296d1490710f96d81307871bac00e02
Frederik Haagensen <frehaa@hotmail.dk>
2017-12-27 19:25:50 +0100

    Add TwoSumFast

---
+++ fundamentals/twoSumFast.py
@@ -0,0 +1,11 @@
+from . import binary_search
+
+def count(a):
+    # Count pairs that sum to 0
+    sorted(a)
+    n = len(a)
+    count = 0
+    for i in range(n):
+        if binary_search.index_of(a, -a[i]) > i:            
+            count += 1
+    return count
