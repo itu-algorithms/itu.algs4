2fe7abf3aa097a6c19b9e43458727c83ff84e8e2
Martino Secchi <smart91k@gmail.com>
2018-01-04 16:52:58 +0100

    lookupCSV using dict() instead of ST

---
+++ searching/LookupCSV.py
@@ -0,0 +1,72 @@
+# Created for BADS 2018
+# See README.md for details
+# Python 3
+import sys, os
+def setpath():
+    exe = sys.argv[0]
+    p = os.path.split(exe)[0]
+    sys.path.insert(0, os.path.join(p, '..', 'stdlib'))
+    sys.path.insert(0, os.path.join(p, '..', 'fundamentals'))
+    sys.path.insert(0, p)
+setpath()
+import stdio
+from queue import Queue
+try:
+    q = Queue()
+    q.enqueue(1)
+except AttributeError:
+    print('ERROR - Could not import algs4 queue')
+    sys.exit(1)
+
+#  Execution:    python LookupCSV file.csv keyField valField
+#  
+#  Reads in a set of key-value pairs from a two-column CSV file
+#  specified on the command line; then, reads in keys from standard
+#  input and prints out corresponding values.
+# 
+#  % python LookupCSV amino.csv 0 3     % python LookupCSV ip.csv 0 1 
+#  TTA                                  www.google.com 
+#  Leucine                              216.239.41.99 
+#  ABC                               
+#  Not found                            % python LookupCSV ip.csv 1 0 
+#  TCT                                  216.239.41.99 
+#  Serine                               www.google.com 
+#                                 
+#  % python LookupCSV amino.csv 3 0     % python LookupCSV DJIA.csv 0 1 
+#  Glycine                              29-Oct-29 
+#  GGG                                  252.38 
+#                                       20-Oct-87 
+#                                       1738.74
+
+"""
+The LookupCSV class provides a data-driven client for reading in a
+key-value pairs from a file; then, printing the values corresponding to the
+keys found on standard input. Both keys and values are strings.
+The fields to serve as the key and value are taken as command-line arguments.
+"""
+
+args = sys.argv[1:]
+keyField = int(args[1])
+valField = int(args[2])
+
+# symbol table
+st = {}
+        #TODO use ST?  ST<String, String> st = new ST<String, String>();
+
+# read in the data from csv file
+file = open(args[0], 'r')
+line = file.readline()
+while line:
+    tokens = line.split(",")
+    key = tokens[keyField]
+    val = tokens[valField]
+    st[key] = val        # st.put(key, val) TODO in case ST
+    line = file.readline()
+
+while not stdio.isEmpty():
+    s = stdio.readString()
+    if s in st:   # st.contains(s)  TODO in case ST 
+        print(st.get(s))
+    else: 
+        print("Not found") 
+file.close()
