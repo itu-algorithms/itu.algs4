e0f8355f0349c06b53f03de51057165f36e1efcb
Frehaa <frehaa@hotmail.dk>
2018-01-09 17:10:02 +0100

    Make TwoSumFast count method static and part of TwoSumFast class

--- fundamentals/twoSumFast.py
+++ fundamentals/twoSumFast.py
@@ -1,11 +1,13 @@
 from . import binary_search
 
-def count(a):
-    # Count pairs that sum to 0
-    sorted(a)
-    n = len(a)
-    count = 0
-    for i in range(n):
-        if binary_search.index_of(a, -a[i]) > i:            
-            count += 1
-    return count
+class TwoSumFast:
+    @staticmethod
+    def count(a):
+        # Count pairs that sum to 0
+        sorted(a)
+        n = len(a)
+        count = 0
+        for i in range(n):
+            if binary_search.index_of(a, -a[i]) > i:            
+                count += 1
+        return count
