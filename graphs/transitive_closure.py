e769df6febffdf630a4f6fa0120f1e29677b8d44
Martino Secchi <smart91k@gmail.com>
2018-01-23 14:24:45 +0100

    transitive closure finished and tested

--- graphs/transitive_closure.py
+++ graphs/transitive_closure.py
@@ -1,14 +1,11 @@
-TransitiveClosure.java
+# Created for BADS 2018
+# See README.md for details
+# Python 3
 
-
-Below is the syntax highlighted version of TransitiveClosure.java.
-
-
-/******************************************************************************
- *  Compilation:  javac TransitiveClosure.java
- *  Execution:    java TransitiveClosure filename.txt
- *  Dependencies: Digraph.java DepthFirstDirectedPaths.java In.java StdOut.java
- *  Data files:   https://algs4.cs.princeton.edu/42digraph/tinyDG.txt
+"""
+ *  Execution:    python transitive_closure.py filename.txt
+ *  Dependencies: Digraph DirectedDFS 
+ *  Data files:   https:#algs4.cs.princeton.edu/42digraph/tinyDG.txt
  *
  *  Compute transitive closure of a digraph and support
  *  reachability queries.
@@ -17,7 +14,7 @@ Below is the syntax highlighted version of TransitiveClosure.java.
  *  Query time: O(1).
  *  Space: O(V^2).
  *
- *  % java TransitiveClosure tinyDG.txt
+ *  % python transitive_closure.py tinyDG.txt
  *         0  1  2  3  4  5  6  7  8  9 10 11 12
  *  --------------------------------------------
  *    0:   T  T  T  T  T  T                     
@@ -34,45 +31,23 @@ Below is the syntax highlighted version of TransitiveClosure.java.
  *   11:   T  T  T  T  T  T           T  T  T  T
  *   12:   T  T  T  T  T  T           T  T  T  T
  *
- ******************************************************************************/
+"""
+import sys
+from graphs.digraph import Digraph
+from graphs.directed_dfs import DirectedDFS
+from errors.errors import IllegalArgumentException
+from stdlib.instream import InStream
 
-package edu.princeton.cs.algs4;
-
-/**
- *  The {@code TransitiveClosure} class represents a data type for 
- *  computing the transitive closure of a digraph.
- *  <p>
- *  This implementation runs depth-first search from each vertex.
- *  The constructor takes time proportional to <em>V</em>(<em>V</em> + <em>E</em>)
- *  (in the worst case) and uses space proportional to <em>V</em><sup>2</sup>,
- *  where <em>V</em> is the number of vertices and <em>E</em> is the number of edges.
- *  <p>
- *  For large digraphs, you may want to consider a more sophisticated algorithm.
- *  <a href = "http://www.cs.hut.fi/~enu/thesis.html">Nuutila</a> proposes two
- *  algorithm for the problem (based on strong components and an interval representation)
- *  that runs in <em>E</em> + <em>V</em> time on typical digraphs.
- *
- *  For additional documentation,
- *  see <a href="https://algs4.cs.princeton.edu/42digraph">Section 4.2</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
- *
- *  @author Robert Sedgewick
- *  @author Kevin Wayne
- */
-public class TransitiveClosure {
-    private DirectedDFS[] tc;  // tc[v] = reachable from v
-
-    /**
+class TransitiveClosure:
+    """
      * Computes the transitive closure of the digraph {@code G}.
      * @param G the digraph
-     */
-    public TransitiveClosure(Digraph G) {
-        tc = new DirectedDFS[G.V()];
-        for (int v = 0; v < G.V(); v++)
-            tc[v] = new DirectedDFS(G, v);
-    }
-
-    /**
+     """
+    def __init__(self, G): 
+        self._tc = [None] * G.V()  # tc[v] = reachable from v
+        for v in range(G.V()):
+            self._tc[v] = DirectedDFS(G, v)
+    """
      * Is there a directed path from vertex {@code v} to vertex {@code w} in the digraph?
      * @param  v the source vertex
      * @param  w the target vertex
@@ -80,74 +55,44 @@ public class TransitiveClosure {
      *         {@code false} otherwise
      * @throws IllegalArgumentException unless {@code 0 <= v < V}
      * @throws IllegalArgumentException unless {@code 0 <= w < V}
-     */
-    public boolean reachable(int v, int w) {
-        validateVertex(v);
-        validateVertex(w);
-        return tc[v].marked(w);
-    }
+     """
+    def reachable(self, v, w):
+        self._validate_vertex(v)
+        self._validate_vertex(w)
+        return self._tc[v].is_marked(w)
 
-    // throw an IllegalArgumentException unless {@code 0 <= v < V}
-    private void validateVertex(int v) {
-        int V = tc.length;
-        if (v < 0 || v >= V)
-            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
-    }
+    # throw an IllegalArgumentException unless {@code 0 <= v < V}
+    def _validate_vertex(self, v):
+        V = len(self._tc)
+        if v < 0 or v >= V:
+            raise IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1))
+    
 
-    /**
-     * Unit tests the {@code TransitiveClosure} data type.
-     *
-     * @param args the command-line arguments
-     */
-    public static void main(String[] args) {
-        In in = new In(args[0]);
-        Digraph G = new Digraph(in);
+def main(args):
+    stream = InStream(args[0])
+    G = Digraph.from_stream(stream)
 
-        TransitiveClosure tc = new TransitiveClosure(G);
+    tc = TransitiveClosure(G)
 
-        // print header
-        StdOut.print("     ");
-        for (int v = 0; v < G.V(); v++)
-            StdOut.printf("%3d", v);
-        StdOut.println();
-        StdOut.println("--------------------------------------------");
+    # print header
+    print("     ", end='')
+    for v in range(G.V()):
+        print("{x:3d}".format(x=v), end='')
+    print()
+    print("--------------------------------------------")
 
-        // print transitive closure
-        for (int v = 0; v < G.V(); v++) {
-            StdOut.printf("%3d: ", v);
-            for (int w = 0; w < G.V(); w++) {
-                if (tc.reachable(v, w)) StdOut.printf("  T");
-                else                    StdOut.printf("   ");
-            }
-            StdOut.println();
-        }
-    }
+    # print transitive closure
+    for v in range(G.V()):
+        print("{x:3d}: ".format(x=v), end='')
+        for w in range(G.V()):
+            if (tc.reachable(v, w)):
+                print("  T", end='')
+            else:
+                print("   ", end='')
+        print()
+    
 
-}
-
-/******************************************************************************
- *  Copyright 2002-2016, Robert Sedgewick and Kevin Wayne.
- *
- *  This file is part of algs4.jar, which accompanies the textbook
- *
- *      Algorithms, 4th edition by Robert Sedgewick and Kevin Wayne,
- *      Addison-Wesley Professional, 2011, ISBN 0-321-57351-X.
- *      http://algs4.cs.princeton.edu
- *
- *
- *  algs4.jar is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  algs4.jar is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with algs4.jar.  If not, see http://www.gnu.org/licenses.
- ******************************************************************************/
+if __name__ == '__main__':
+    main(sys.argv[1:])
 
 
-Last updated: Mon Dec 11 03:54:04 EST 2017.
