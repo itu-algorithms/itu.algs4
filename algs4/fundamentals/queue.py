e58e1d36ed86aece1a7b20c4dd2c0215cb1f67a5
Thore Husfeldt <thore.husfeldt@gmail.com>
2020-01-09 10:44:16 +0100

    [queue.py] Add types

--- algs4/fundamentals/queue.py
+++ algs4/fundamentals/queue.py
@@ -1,54 +1,57 @@
-import sys
-from algs4.stdlib import stdio
-from algs4.errors.errors import NoSuchElementException
+from ..errors.errors import NoSuchElementException
 
 # Created for BADS 2018
 # See README.md for details
 # This is python3
 
+from typing import Generic, Optional, TypeVar, Iterator
 
-class Queue:
+T = TypeVar('T')
+
+class Node(Generic[T]):
+    def __init__(self, item: T, next: Optional['Node[T]']) -> None:
+        """
+        Initializes a new node
+        :param item: the item to be stored in the node
+        :param next: the next node in the queue
+        """
+        self.item: T = item
+        self.next: Optional[Node[T]] = next
+
+class Queue(Generic[T]):
     """
     The Queue class represents a first-in-first-out (FIFO)
     queue of generic items.
     It supports the usual enqueue and dequeue
     operations, along with methods for peeking at the first item,
     testing if the queue is empty, and iterating through the items in FIFO order
-    This implementation uses a singly linked list with a nested class for the linked-list nodes
+    This implementation uses a singly linked list of linked-list nodes
     The enqueue, dequeue, peek, size, and is_empty operations all take constant time in the worst case
     """
-    class Node:
-        def __init__(self, item, next):
-            """
-            Initializes a new node
-            :param item: the item to be stored in the node
-            :param next: the next node in the queue
-            """
-            self.item = item
-            self.next = next
 
-    def __init__(self):
+    def __init__(self) -> None:
         """
         Initializes an empty queue.
         """
-        self._first = None
-        self._last = None
-        self._n = 0
+        self._first: Optional[Node[T]] = None
+        self._last : Optional[Node[T]] = None
+        self._n: int = 0
 
-    def enqueue(self, item):
+    def enqueue(self, item: T) -> None:
         """
         Adds the item to this queue.
         :param item: the item to add
         """
-        old_last = self._last
-        self._last = self.Node(item, None)
+        old_last: Optional[Node[T]] = self._last
+        self._last = Node(item, None)
         if self.is_empty():
             self._first = self._last
         else:
+            assert old_last is not None
             old_last.next = self._last
         self._n += 1
 
-    def dequeue(self):
+    def dequeue(self) -> T:
         """
         Removes and returns the item on this queue that was least recently added.
         :return: the item on this queue that was least recently added.
@@ -57,6 +60,7 @@ class Queue:
         if self.is_empty():
             raise NoSuchElementException("Queue underflow")
 
+        assert self._first is not None
         item = self._first.item
         self._first = self._first.next
         self._n -= 1
@@ -64,7 +68,7 @@ class Queue:
             self._last = None
         return item
 
-    def is_empty(self):
+    def is_empty(self) -> bool:
         """
         Returns true if this queue is empty.
         :return: True if this queue is empty otherwise False
@@ -72,7 +76,7 @@ class Queue:
         """
         return self._first is None
 
-    def size(self):
+    def size(self) -> int:
         """
         Returns the number of items in this queue.
         :return: the number of items in this queue
@@ -80,10 +84,10 @@ class Queue:
         """
         return self._n
 
-    def __len__(self):
+    def __len__(self) -> int:
         return self.size()
 
-    def peek(self):
+    def peek(self) -> T:
         """
         Returns the item least recently added to this queue.
         :return: the item least recently added to this queue
@@ -92,9 +96,10 @@ class Queue:
         if self.is_empty():
             raise NoSuchElementException("Queue underflow")
 
+        assert self._first is not None
         return self._first.item
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[T]:
         """
         Iterates over all the items in this queue in FIFO order.
         """
@@ -103,7 +108,7 @@ class Queue:
             yield curr.item
             curr = curr.next
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         """
         Returns a string representation of this queue.
         :return: the sequence of items in FIFO order, separated by spaces
@@ -112,23 +117,3 @@ class Queue:
         for item in self:
             s.append("{} ".format(item))
         return ''.join(s)
-
-
-def main():
-    """
-    Reads strings from an stdin and adds them to a queue.
-    When reading a '-' it removes the least recently added item and prints it.
-    Prints the amount of items left on the queue.
-    """
-    queue = Queue()
-    while not stdio.isEmpty():
-        input_item = stdio.readString()
-        if input_item != '-':
-            queue.enqueue(input_item)
-        elif not queue.is_empty():
-            print(queue.dequeue())
-    print('({} left on queue)'.format(queue.size()))
-
-
-if __name__ == '__main__':
-    main()
---
+++ examples/queue.py
@@ -0,0 +1,16 @@
+from algs4.fundamentals.queue import Queue
+import algs4.stdlib.stdio
+
+"""
+Reads strings from an stdin and adds them to a queue.
+When reading a '-' it removes the least recently added item and prints it.
+Prints the amount of items left on the queue.
+"""
+queue = Queue()
+while not stdio.isEmpty():
+    input_item = stdio.readString()
+    if input_item != '-':
+        queue.enqueue(input_item)
+    elif not queue.is_empty():
+        print(queue.dequeue())
+print('({} left on queue)'.format(queue.size()))
