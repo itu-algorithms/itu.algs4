e9646aab89e2c623df06fd6cbc303efebfc2bd9d
Holger Dell <holgerdell@users.noreply.github.com>
2020-05-16 12:49:00 +0200

    testing: added symbol table test to expose issue #2.

--- itu/algs4/searching/st.py
+++ itu/algs4/searching/st.py
@@ -1,16 +1,14 @@
 # Created for BADS 2018
 # See README.md for details
 # Python 3
-import sys
-
 from itu.algs4.errors.errors import IllegalArgumentException, NoSuchElementException
-from itu.algs4.stdlib import stdio
 
 
 """
  The ST class represents an ordered symbol table of generic
  key-value pairs.
 """
+
 
 class ST:
     def __init__(self):
@@ -19,10 +17,10 @@ class ST:
     # Returns the value associated with the given key in this symbol table.
     def get(self, key):
         if key is None:
-           raise IllegalArgumentException("called get() with None key")
+            raise IllegalArgumentException("called get() with None key")
         return self._st.get(key)
 
-    # Inserts the specified key-value pair into the symbol table, overwriting the old 
+    # Inserts the specified key-value pair into the symbol table, overwriting the old
     # value with the new value if the symbol table already contains the specified key.
     # Deletes the specified key (and its associated value) from this symbol table
     # if the specified value is None.
@@ -34,7 +32,7 @@ class ST:
         else:
             self._st[key] = val
 
-    # Removes the specified key and its associated value from this symbol table     
+    # Removes the specified key and its associated value from this symbol table
     # (if the key is in this symbol table).
     def delete(self, key):
         if key is None:
@@ -87,7 +85,9 @@ class ST:
         keys = self.keys()
         ceiling = None
         for k in keys:
-            if (ceiling is None and k >= key) or (ceiling is not None and k>=key and k<ceiling):
+            if (ceiling is None and k >= key) or (
+                ceiling is not None and k >= key and k < ceiling
+            ):
                 ceiling = k
         if ceiling is None:
             raise NoSuchElementException("all keys are less than " + str(key))
@@ -100,42 +100,10 @@ class ST:
         keys = self.keys()
         floor = None
         for k in keys:
-            if (floor is None and k <= key) or (floor is not None and k<=key and k>floor):
+            if (floor is None and k <= key) or (
+                floor is not None and k <= key and k > floor
+            ):
                 floor = k
         if floor is None:
             raise NoSuchElementException("all keys are greater than " + str(key))
         return floor
-
-def test():
-    st = ST()
-    st.put("one", 1)
-    assert ["one"] == list(st.keys()) 
-    assert st.get("one") == 1
-    assert st.contains("one")
-    st.delete("one")
-    assert st.is_empty()
-    st.put("aaa", 1)
-    st.put("bbb", 2)
-    st.put("ccc", 3)
-    st.put("ddd", 4)
-    st.put("eee", 5)
-    assert st.ceiling("ccc") == "ccc"
-    assert st.ceiling("dad") == "ddd"
-    assert st.floor("ccc") == "ccc"
-    assert st.floor("dad") == "ccc"
-    for k in st:
-        assert k in st.keys()
-    assert st.min() == "aaa"
-    assert st.max() == "eee"
-    print("tests passed.")
-
-if __name__ == '__main__':
-    test()
-    st = ST()
-    i = 0
-    while not stdio.isEmpty():
-        key = stdio.readString()
-        st.put(key, i)
-        i += 1
-    for s in st.keys():
-        print(s + " " + str(st.get(s)))
---
+++ tests/test_symbol_tables.py
@@ -0,0 +1,68 @@
+# import random
+#
+import pytest
+
+from itu.algs4.searching.binary_search_st import BinarySearchST
+from itu.algs4.searching.bst import BST
+from itu.algs4.searching.linear_probing_hst import LinearProbingHashST
+from itu.algs4.searching.red_black_bst import RedBlackBST
+from itu.algs4.searching.seperate_chaining_hst import SeparateChainingHashST
+from itu.algs4.searching.sequential_search_st import SequentialSearchST
+from itu.algs4.searching.st import ST
+
+
+ST_IMPLEMENTATIONS = [
+    BinarySearchST,
+    BST,
+    LinearProbingHashST,
+    RedBlackBST,
+    SeparateChainingHashST,
+    SequentialSearchST,
+    ST,
+]
+
+
+@pytest.mark.parametrize(
+    "st", [constructor() for constructor in ST_IMPLEMENTATIONS],
+)
+def test_is_empty(st):
+    assert st.is_empty()
+
+
+@pytest.mark.parametrize(
+    "st", [constructor() for constructor in ST_IMPLEMENTATIONS],
+)
+def test_delete(st):
+    st.put("key1", "val1")
+    st.put("key2", "val2")
+    st.put("key3", "val3")
+    st.delete("key3")
+    st.delete("key2")
+    st.delete("key1")
+    assert st.is_empty()
+
+
+@pytest.mark.parametrize(
+    "st", [constructor() for constructor in ST_IMPLEMENTATIONS],
+)
+def old_tests(st):
+    st.put("one", 1)
+    assert ["one"] == list(st.keys())
+    assert st.get("one") == 1
+    assert st.contains("one")
+    st.delete("one")
+    assert st.is_empty()
+    st.put("aaa", 1)
+    st.put("bbb", 2)
+    st.put("ccc", 3)
+    st.put("ddd", 4)
+    st.put("eee", 5)
+    if st.ceiling:
+        assert st.ceiling("ccc") == "ccc"
+        assert st.ceiling("dad") == "ddd"
+        assert st.floor("ccc") == "ccc"
+        assert st.floor("dad") == "ccc"
+    for k in st:
+        assert k in st.keys()
+    assert st.min() == "aaa"
+    assert st.max() == "eee"
